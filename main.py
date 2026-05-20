48597faaf6dbce7d81133bae9085f7e6e2280dd3
AGaynetdinov <AGaynetdinov@IBS.RU>
2026-05-20 15:40:52 +0300

    5.1

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
-from .schemas import STaskAdd, STask
+from fastapi import FastAPI
+from .router import router as tasks_router
 
 app = FastAPI()
 
-tasks = []
-
-@app.post("/tasks", status_code=status.HTTP_201_CREATED)
-async def create_task(task: STaskAdd) -> STask:
-    task_dict = task.model_dump()
-    task_dict["id"] = 1
-    tasks.append(task_dict)
-    return task_dict
-
-@app.get("/tasks/{task_id}", response_model=STask)
-async def get_task(task_id: int):
-    for task in tasks:
-        if task["id"] == task_id:
-            return task
-    
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Задача с ID {task_id} не найдена"
-    )
-
-@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_task(task_id: int):
-    for index, task in enumerate(tasks):
-        if task_id == task["id"]:
-            tasks.pop(index)
-            return
-    
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Задача с ID {task_id} не найдена"
-    )
+app.include_router(tasks_router)
---
+++ router.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, HTTPException, status
+from .schemas import STaskAdd, STask
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["Задачи"]
+)
+
+tasks = []
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_task(task: STaskAdd) -> STask:
+    task_dict = task.model_dump()
+    task_dict["id"] = 1
+    tasks.append(task_dict)
+    return task_dict
+
+@router.get("/{task_id}", response_model=STask)
+async def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Задача с ID {task_id} не найдена"
+    )
+
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task_id == task["id"]:
+            tasks.pop(index)
+            return
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Задача с ID {task_id} не найдена"
+    )
